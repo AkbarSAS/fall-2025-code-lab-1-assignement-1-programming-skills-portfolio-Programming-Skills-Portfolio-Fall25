bd2b1d8ddf2b849e4b32978aa356643778341a12
Akbar Aman <akbaraman200@gmail.com>
2025-11-20 14:31:23 +0400

    Solution 02- Simple Sums
    Assign integer values to variables x and y, calculate their sum and store it in variable z, and print the value of the sum.

---
+++ Exercise 02- Simple Sums/Solution 02- Simple Sums.py
@@ -0,0 +1,11 @@
+# Assign value 8 to variable x
+x = 8
+
+# Assign value 10 to variable y
+y = 10
+
+# Calculate the sum and store it in variable z
+z = x + y
+
+# Print the value of the sum to the console
+print (z)
