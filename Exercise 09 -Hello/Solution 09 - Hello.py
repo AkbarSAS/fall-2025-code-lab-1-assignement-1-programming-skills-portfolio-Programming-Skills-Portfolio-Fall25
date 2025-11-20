a15223183ce54cfd0630aaa69f19c15f90f1ead0
Akbar Aman <akbaraman200@gmail.com>
2025-11-20 16:10:56 +0400

    Solution 09 - Hello
    This program defines a function that prints Hello and calls it through a main function.

---
+++ Exercise 09 -Hello/Solution 09 - Hello.py
@@ -0,0 +1,10 @@
+# Define a function to print 'Hello' to the console
+def hello():
+    print ('Hello') # Display the message 'Hello'
+    
+# Define the main function that calls the hello() function
+def main():
+    hello() # Call the hello() function
+
+if __name__ == "__main__":
+    main()
