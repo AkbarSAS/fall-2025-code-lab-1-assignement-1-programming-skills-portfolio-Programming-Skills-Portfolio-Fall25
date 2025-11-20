313fb5e5a23e91b1fa4eadb1bfb826bd67fee5c6
Akbar Aman <akbaraman200@gmail.com>
2025-11-20 14:16:07 +0400

    Solution 01 - Coding is Cool
    Assign strings to variables and print the words in a single line using string concatenation.

---
+++ Exercise 01 - Coding is Cool/Solution 01 - Coding is Cool.py
@@ -0,0 +1,7 @@
+# Assign strings to the variables
+word1 = 'Coding'
+word2 = 'is'
+word3 = 'Cool'
+
+# Use string concatenation to print the words in a single line
+print (word1 + " " + word2 + " " + word3)
