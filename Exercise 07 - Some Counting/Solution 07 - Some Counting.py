31169785484dfa3ab6a46743d804e77c3e536afa
Akbar Aman <akbaraman200@gmail.com>
2025-11-20 16:00:51 +0400

    Solution 07 - Some Counting
    This program uses for loops to count both upward and downward across different ranges. Counting up in increments and counting down in decrements. Display the output in the console.

---
+++ Exercise 07 - Some Counting/Solution 07 - Some Counting.py
@@ -0,0 +1,24 @@
+# Counting up from 0 to 50 in increments of 1
+print ('\nLoop 1: Count up from 0 to 50')
+for i in range (0, 51, 1): # Start from 0 and end with 50 with step as 1
+    print (i)
+
+# counting down from 50 to 0 in decrements of 1
+print (' \nLoop 2: Count down from 50 to 0')
+for i in range (50, -1, -1): # Start from 50 and end with 0 with step as -1
+    print (i)
+
+# counting up from 30 to 50 in increments of 1
+print (' \nLoop 3: Count up from 30 to 50')
+for i in range (30, 51, 1): # Start from 30 and end with 50 with step as 1
+    print (i)
+
+# counting down from 50 to 10 in decrements of 2
+print (' \nLoop 4: Count down from 50 to 10')
+for i in range (50, 9, -2): # Start from 50 and end with 10 with step as -2
+    print (i)
+
+# counting up from 100 to 200 in increments of 5
+print (' \nLoop 5: Count up from 100 to 200')
+for i in range (100, 201, 5): # Start from 100 and end with 201 with step as 5
+    print (i)
